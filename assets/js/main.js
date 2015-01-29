d52d8d39e481f4820ed053556b4c05fefed1e7be
Dave Lester <dave@golfbert.com>
2015-01-28 19:00:40 -0800

    Added sign in/up, forgot PW, and re-worked search

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,25 @@
 $(document).ready(function() {
 	
+	$('.ad-block').load('footer.html');
+	
+	// Toggle map and list
 	$(".results-click").click(function() {
 		$(".results, .results-click").toggle();
+	});
+	
+	// Search
+	$(".search-wrapper input").click(function() {
+		$(".inputs li:nth-of-type(2), .inputs li:last-of-type").hide();
+		$(".inputs li.search-wrapper").css("width","96%");
+		$(".header-overlay, .header-exit, .geo-search").show();
+		$('.results-search-wrapper').load('search.html');
+	});
+	
+	$(".header-exit").click(function() {
+		$(".inputs li:nth-of-type(2), .inputs li:last-of-type").show();
+		$(".inputs li.search-wrapper").css("width","calc(100% - 100px)");
+		$(".header-overlay, .header-exit, .geo-search").hide();
+		$('.results-search-wrapper').empty('search.html');
 	});
 	
 })
